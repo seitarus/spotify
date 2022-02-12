7bcd340cc642a8ed5008ecb1716619809fea355c
Alex <alejandro@jig.es>
2022-02-12 12:07:22 +0100

    Vista de Album

---
+++ Spotify/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by iMac 27 iOS on 12/2/22.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
