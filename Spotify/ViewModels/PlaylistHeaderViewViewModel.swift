8f4c852ab9a1a42d5ad3b564cdb184d0d85b1d3c
Alex <alejandro@jig.es>
2022-02-12 11:15:39 +0100

    Vista de Playlists

---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by iMac 27 iOS on 12/2/22.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
